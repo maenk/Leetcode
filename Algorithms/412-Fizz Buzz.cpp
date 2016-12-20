1c05eb9e72811266ac761998316121783e310472
maenk <mayank.sareen41@gmail.com>
2016-12-20 11:06:24 +0530

    Add files via upload
    412.FizzBuzz

---
+++ Algorithms/412-Fizz Buzz.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> vec;
+        for (int i=1; i<=n; ++i)
+        {
+            if ((i%5==0)&&(i%3==0)) vec.push_back("FizzBuzz");
+            else if (i%5==0) vec.push_back("Buzz");
+            else if (i%3==0) vec.push_back("Fizz");
+            else vec.push_back(to_string(i));
+        }
+            return vec;
+    }
+};
